d59f365101313d738cf8c9354fd3f65724dbb638
Vijay Chavan <147536828+chavanvijay01@users.noreply.github.com>
2024-02-15 14:27:34 +0530

    Update script.js

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -1,10 +1,17 @@
 async function fetchWeatherData(cityName){
-    // let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=448b136ce36df3e3b08147b91500b62b`
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=448b136ce36df3e3b08147b91500b62b`
     
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=448b136ce36df3e3b08147b91500b62b`
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
+    let city = document.getElementById("city");
+    city.innerHTML = data.name;
+    let windSpeed = document.getElementById("wind_speed");
+    windSpeed.innerHTML = data.wind.speed + "km/hr";
+    let humidity = document.getElementById("humidity");
+    humidity.innerHTML = data.main.humidity + "g/kg";
+    let temp = document.getElementById("temp");
+    temp.innerHTML = Math.round((data.main.temp) - 273.15) + "°C";
+    console.log(data);
 }
 
 
